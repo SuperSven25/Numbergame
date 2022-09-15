317f2d49169af3ed7b2ebf970113ad83d65a8d28
Alexander Svensson <alexander.svensson@learnet.se>
2022-09-15 11:00:39 +0200

    klar

--- Numbergame.js
+++ Numbergame.js
@@ -1,27 +1,29 @@
 var ps = require('prompt-sync');
 var prompt = ps();
+console.log("yo")
+console.log("You have 3 lives to guess")
 
 
-function Numbergame ()
+function Numbergame (){
 
 const random = Math.floor(Math.random() * 10) + 1;
  
- let tries = 3
+ let tries = 2;
 
  let number;
- while(number !== random){
-    let number=parseInt(prompt("guess the number 1 to 10: "))
-    if(number==random){
-        console.log("You guessed correctly!")
-        break
- } else if (number<random){
-    let number=parseInt(prompt("Try higher: "))
- }else {
-    let number=parseInt(prompt("Try lower: "))
-    break
+
+ while(number !== random) {
+    let number = parseInt(prompt("guess the number 1 to 10: "));
+    if(number === random) {
+        console.log("You guessed correctly!.")
+        break;
+    } else if (tries === 0) {
+    console.log("Your lives are out ")
+    break;
+    } else {
+    console.log("Wrong ")
+    tries--;
+    }
+
  }
-
- 
-}
-
-
+} Numbergame()
